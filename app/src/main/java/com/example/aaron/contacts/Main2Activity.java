21344b679d83d95694b52f0694e6088817d518ed
aaronhoskins2011 <aaronhoskins2011@gmail.com>
2017-08-20 17:04:07 -0500

    Corrected passing object from Main to Two

--- app/src/main/java/com/example/aaron/contacts/Main2Activity.java
+++ app/src/main/java/com/example/aaron/contacts/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.aaron.contacts;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,25 +13,22 @@ import static android.R.id.list;
 
 
 public class Main2Activity extends AppCompatActivity {
+    private static final String TAG = "ActivityTwo";
     ListView contactListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: 2nd Activity");;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Intent intent = new Intent();
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
         contactListView = (ListView)findViewById(R.id.contactListView);
         AdapterContact adapterContact;
-        final ArrayList<Contacts> contactList = intent.getParcelableArrayListExtra("contacts");
-
-        adapterContact = new AdapterContact(this,0,contactList);
+        final ArrayList<Contacts> contactList = b.getParcelableArrayList("contact");
+        Log.d(TAG, "onCreate: CONTACT LIST ------ " + contactList.get(0).getLastName());
+        adapterContact = new AdapterContact(this,1,contactList);
         contactListView.setAdapter(adapterContact);
-
-
-
-
-
-
 
     }
 }
